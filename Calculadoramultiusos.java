76da8eb8cbe494be60f4e5541eea87f26698ef45
Fabeovski <fabian.rojque@sanviatorvalladolid.com>
2026-09-24 13:23:15 +0200

    se ñade la calculadora multiusos

---
+++ Calculadoramultiusos.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Calculadoramultiusos {
+	
+	public static void main(String[] args) {
+	
+		/*
+	Scanner teclado = new Scanner(System.in);
+	System.out.println("Fabian");
+	System.out.println("¿Que edad tienes?");
+	int numero1 = teclado.nextInt();
+	int numero2 = 18;
+	
+	if (numero1 > numero2) {
+		System.out.println("eres mayor de edad");
+	} else if (numero2 > numero1) {
+		System.out.println("mal eres menor de edad");
+	} else if (numero1 == numero2) {
+		System.out.println("Son iguales");
+	}
+	
+	teclado.close();
+	*/
+		
+		/*
+	Scanner teclado = new Scanner(System.in);
+	System.out.println("Fabian");
+	System.out.println("¿en que año naciste?");
+	int n1 = teclado.nextInt();
+	int n2 = 2026;
+	int resultado = n2-n1; 
+	
+	if (n1 < n2) {
+		System.out.println("eres mayor de edad");
+		System.out.println("porque tienes "+resultado);
+	} else if (n2 > n1) {
+		System.out.println("mal eres menor de edad");
+	} else if (n1 == n2) {
+		System.out.println("Son iguales");
+	}
+	teclado.close();
+	}
+}
+*/
+		/*
+		Scanner teclado = new Scanner(System.in);
+		
+		int año = 2026;
+		
+		System.out.println("Fabian");
+		System.out.println("En que año naciste?");
+		int n1 = teclado.nextInt();
+		int resultado = año-n1;
+		System.out.println("tienes entonces "+resultado+ " de edad");
+		
+		System.out.println("paco");
+		System.out.println("En que año nacio paco?");
+		int n2 = teclado.nextInt();
+		int resultado2 = año-n2;
+		System.out.println("tienes entonces "+resultado2+ " de edad");
+				
+		if (n1 > n2) {
+			System.out.println("Eres mayor que paco");
+		} else if (n2 > n1) {
+			System.out.println("Eres Menor que fabian");
+		} else if (n1 == n2) {
+			System.out.println("tienen la misma edad");
+		}
+		teclado.close();
+		*/
+		}
+	}
